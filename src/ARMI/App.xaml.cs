d030a4bd9fe635a62b2a54117361a61ceeb12b7a
Rolf Veinø Sørensen <48915559+RetroTheArcadian@users.noreply.github.com>
2022-07-06 09:21:39 +0200

    create basic maui app

---
+++ src/ARMI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ARMI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/ARMI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ARMI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
